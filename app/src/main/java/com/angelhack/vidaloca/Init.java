ce4e924be123efb94455e5d7766873a4769a67c3
NilayKulkarni <typhoscientic@gmail.com>
2015-05-16 15:00:23 +0530

    Added dependencies

---
+++ app/src/main/java/com/angelhack/vidaloca/Init.java
@@ -0,0 +1,8 @@
+package com.angelhack.vidaloca;
+
+/**
+ * Created by geek on 16/5/15.
+ */
+public class Init {
+    public static String api_key = "277ed45d-46ca-4957-941c-12189dea5c48";
+}
--- app/src/main/java/com/angelhack/vidaloca/MainActivity.java
+++ app/src/main/java/com/angelhack/vidaloca/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.angelhack.vidaloca.Init.*;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    String api = Init.api_key;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
