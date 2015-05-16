e7f41e1c4debb822ec109d4119876b8b09db035d
NilayKulkarni <typhoscientic@gmail.com>
2015-05-16 17:51:53 +0530

    Added querytext prototype for testing

--- app/src/main/java/com/angelhack/vidaloca/Init.java
+++ app/src/main/java/com/angelhack/vidaloca/Init.java
@@ -5,5 +5,5 @@ package com.angelhack.vidaloca;
  */
 public class Init {
     public static String api_key = "277ed45d-46ca-4957-941c-12189dea5c48";
-    public static String url = "https://api.idolondemand.com/1/api/async/recognizespeech/v1?interval=10&file=/storage/external_SD/DCIM/100LGDSC/CAM00385.mp4";
+    public static String url = "https://api.idolondemand.com/1/api/sync/querytextindex/v1";
 }
--- app/src/main/java/com/angelhack/vidaloca/MainActivity.java
+++ app/src/main/java/com/angelhack/vidaloca/MainActivity.java
@@ -4,24 +4,80 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.util.EntityUtils;
 import com.angelhack.vidaloca.Init.*;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 
 public class MainActivity extends ActionBarActivity {
 
     Button up;
     String api = Init.api_key;
     String video = Init.url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        up = (Button) findViewById(R.id.up);
-
+        Client1 client1 = new Client1();
+        client1.run();
     }
 
+    class Client1 {
+        public void run() {
+
+            org.apache.http.client.HttpClient httpClient = new DefaultHttpClient();
+            video += "?apikey="+api+"&text=genome";
+            HttpGet httpGet = new HttpGet(video);
+            CloseableHttpResponse response = null;
+            try {
+                response = (CloseableHttpResponse) httpClient.execute(httpGet);
+                org.apache.http.StatusLine statusLine = response.getStatusLine();
+                Toast.makeText(MainActivity.this, "StatusLine: " + statusLine, Toast.LENGTH_SHORT).show();
+                HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    long len = entity.getContentLength();
+                    if (len != -1 && len < 2048) {
+                        String body = EntityUtils.toString(entity);
+                        Toast.makeText(MainActivity.this, body, Toast.LENGTH_SHORT).show();
+                    } else {
+
+                    }
+                }
+                }catch (ClientProtocolException cpe){
+                cpe.printStackTrace();
+            } catch (IOException ioe){
+                ioe.printStackTrace();
+            } finally {
+                try {
+                    response.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
